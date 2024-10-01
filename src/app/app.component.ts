3bcfcb212ab5ce10a2808c9c7eb1475eec269028
jeremy <projeremylegendre@gmail.com>
2024-10-01 13:18:49 +0200

    nettoyage du code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { Exercice_Annuel_Service } from './services/exercices_annuels.service';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { Exercice_annuelList } from './data/exercices_annuels-model';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -14,13 +13,12 @@ export class AppComponent {
   title = 'SassyFront';
   readonly Exercice_Annuel_Service = inject(Exercice_Annuel_Service);
   readonly ListeExerciceAnnuel = toSignal(
-    this.Exercice_Annuel_Service.getAllExerciceAnnuel(),
-    { initialValue: [] }
+    this.Exercice_Annuel_Service.getAllExerciceAnnuel()
   );
   readonly TwoLastExercice = computed(() => {
     const liste = this.ListeExerciceAnnuel(); // Appelle une fois
     console.log(liste);
     return liste ? liste.slice(-2) : [];
   });
-  //readonly Loader = computed(() => !this.ListeExerciceAnnuel());
+  readonly Loader = computed(() => !this.ListeExerciceAnnuel());
 }
