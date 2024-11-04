e4338f363c0b5d449f43ea78a6ccc47129beaaf2
jeremy <projeremylegendre@gmail.com>
2024-11-04 16:39:57 +0100

    add last client in home page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { clientsService } from '../services/http/clients.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { catchError, map, of } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -9,4 +11,21 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home-page.component.html',
   styles: ``,
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  readonly clientservice = inject(clientsService);
+  private readonly clientListResponse = toSignal(
+    this.clientservice.getAllClients().pipe(
+      map((value) => ({ value, error: undefined })),
+      catchError((error) => of({ value: undefined, error }))
+    )
+  );
+  readonly lastClientLoader = computed(()=>!this.clientListResponse());
+  readonly lastClientOnTheList = computed(() => {
+    const data = this.clientListResponse()?.value;
+    if (data) {
+      return data.at(-1);
+    } else {
+      return null;
+    }
+  });
+}
