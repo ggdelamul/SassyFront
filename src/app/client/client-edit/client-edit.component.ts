033132a0f6de1077652f935bbed91183414ae470
jeremy <projeremylegendre@gmail.com>
2024-11-04 15:57:32 +0100

    mep en place edit client et delete client

--- src/app/client/client-edit/client-edit.component.ts
+++ src/app/client/client-edit/client-edit.component.ts
@@ -1,5 +1,5 @@
-import { Component, computed, effect, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, computed, effect, inject, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { clientsService } from '../../services/http/clients.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import {
@@ -9,6 +9,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { catchError, map, of } from 'rxjs';
+import { Client, CLIENT_RULES, StatutClient } from '../../data/client-model';
 @Component({
   selector: 'app-client-edit',
   standalone: true,
@@ -18,6 +19,7 @@ import { catchError, map, of } from 'rxjs';
 })
 export class ClientEditComponent {
   readonly clientService = inject(clientsService);
+  readonly router = inject(Router);
   readonly route = inject(ActivatedRoute);
   readonly idClient = Number(this.route.snapshot.paramMap.get('id'));
   // Notre nouveau Signal avec la réponse HTTP "brute".
@@ -30,23 +32,40 @@ export class ClientEditComponent {
   readonly loading = computed(() => !this.clientResponse());
   readonly error = computed(() => this.clientResponse()?.error);
   readonly client = computed(() => this.clientResponse()?.value);
-
+  readonly status = Object.values(StatutClient);
   readonly form = new FormGroup({
-    raisonsociale: new FormControl(),
-    adressemail: new FormControl(),
-    telphone: new FormControl(),
-    adresse: new FormControl(),
-    statut: new FormControl(),
+    raisonsociale: new FormControl('', [
+      Validators.required,
+      Validators.minLength(CLIENT_RULES.RAISON_SOCIALE.MIN_LENGTH),
+      Validators.maxLength(CLIENT_RULES.RAISON_SOCIALE.MAX_LENGTH),
+      Validators.pattern(CLIENT_RULES.RAISON_SOCIALE.NAME_PATTERN),
+    ]),
+    adressemail: new FormControl(
+      '',
+      Validators.pattern(CLIENT_RULES.MAIL.MAIL_PATTERN)
+    ),
+    telephone: new FormControl(
+      '',
+      Validators.minLength(CLIENT_RULES.TELEPHONE.MIN_LENGTH)
+    ),
+    adresse: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(255),
+    ]),
+    statut: new FormControl(''),
   });
+  readonly CLIENT_RULES = signal(CLIENT_RULES).asReadonly();
+
   constructor() {
-    // Utilisation d'un effet pour mettre à jour le formulaire une fois les données disponibles
     effect(() => {
       const client = this.client();
       if (client) {
+        FormControl;
         this.form.patchValue({
           raisonsociale: client.raisonsociale,
-          adressemail: client.adressemail,
-          telphone: client.telephone,
+          adressemail: client.mail,
+          telephone: client.telephone,
           adresse: client.adresse,
           statut: client.statut,
         });
@@ -54,6 +73,38 @@ export class ClientEditComponent {
     });
   }
   onSubmit() {
-    console.log('soumis');
+    const isvalid = this.form.valid;
+    const clientForsubmit = this.client();
+    if (isvalid && clientForsubmit) {
+      const updatedClient: Client = {
+        ...clientForsubmit,
+        raisonsociale: this.raisonsociale.value as string,
+        mail: this.adressemail.value as string,
+        telephone: this.telephone.value as string,
+        adresse: this.adresse.value as string,
+        statut: this.statut.value as StatutClient,
+      };
+      console.log(this.statut.value);
+      console.log(updatedClient);
+      this.clientService.updatedClient(updatedClient).subscribe(() => {
+        this.router.navigate(['/client', this.client()?.id]);
+      });
+    }
+  }
+  // Getter pour les controle de formulaire
+  get raisonsociale() {
+    return this.form.get('raisonsociale') as FormControl;
+  }
+  get adressemail() {
+    return this.form.get('adressemail') as FormControl;
+  }
+  get telephone() {
+    return this.form.get('telephone') as FormControl;
+  }
+  get adresse() {
+    return this.form.get('adresse') as FormControl;
+  }
+  get statut() {
+    return this.form.get('statut') as FormControl;
   }
 }
--- src/app/data/client-model.ts
+++ src/app/data/client-model.ts
@@ -7,9 +7,27 @@ export interface Client {
   id: number;
   raisonsociale: string;
   datecreation: string;
-  adressemail: string;
+  mail: string;
   telephone: string;
   adresse: string;
   statut: StatutClient;
 }
 export type ClientList = Client[];
+
+export const CLIENT_RULES = {
+  RAISON_SOCIALE: {
+    NAME_PATTERN: /^[a-zA-Zéè]+$/,
+    MAX_LENGTH: 255,
+    MIN_LENGTH: 3,
+  },
+  MAIL: {
+    MAIL_PATTERN: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+  },
+  TELEPHONE: {
+    MIN_LENGTH: 10,
+  },
+  ADRESSE: {
+    MAX_LENGTH: 255,
+    MIN_LENGTH: 5,
+  },
+} as const;
